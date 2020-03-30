49fc060591056aaaf5fd25e4760c97ce5e67005e
1119178517 <1119178517@qq.com>
2020-03-30 18:33:38 +0800

    week2.5

---
+++ week2.5获取鼠标点击位置/week2.h
@@ -0,0 +1,33 @@
+﻿
+// week2.h: week2 应用程序的主头文件
+//
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'pch.h' before including this file for PCH"
+#endif
+
+#include "resource.h"       // 主符号
+
+
+// Cweek2App:
+// 有关此类的实现，请参阅 week2.cpp
+//
+
+class Cweek2App : public CWinApp
+{
+public:
+	Cweek2App() noexcept;
+
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+	virtual int ExitInstance();
+
+// 实现
+	afx_msg void OnAppAbout();
+	DECLARE_MESSAGE_MAP()
+};
+
+extern Cweek2App theApp;
