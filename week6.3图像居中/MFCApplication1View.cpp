6c754af5c67b918de6a2d13d6e5efa4537ba0e62
1119178517 <1119178517@qq.com>
2020-03-31 14:45:29 +0800

    week6.3

---
+++ week6.3图像居中/MFCApplication1View.cpp
@@ -0,0 +1,125 @@
+﻿
+// MFCApplication1View.cpp: CMFCApplication1View 类的实现
+//
+
+#include "pch.h"
+#include "framework.h"
+// SHARED_HANDLERS 可以在实现预览、缩略图和搜索筛选器句柄的
+// ATL 项目中进行定义，并允许与该项目共享文档代码。
+#ifndef SHARED_HANDLERS
+#include "MFCApplication1.h"
+#endif
+
+#include "MFCApplication1Doc.h"
+#include "MFCApplication1View.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CMFCApplication1View
+
+IMPLEMENT_DYNCREATE(CMFCApplication1View, CView)
+
+BEGIN_MESSAGE_MAP(CMFCApplication1View, CView)
+	ON_COMMAND(ID_FILE_OPEN, &CMFCApplication1View::OnFileOpen)
+END_MESSAGE_MAP()
+
+// CMFCApplication1View 构造/析构
+
+CMFCApplication1View::CMFCApplication1View() noexcept
+{
+	// TODO: 在此处添加构造代码
+
+}
+
+CMFCApplication1View::~CMFCApplication1View()
+{
+}
+
+BOOL CMFCApplication1View::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: 在此处通过修改
+	//  CREATESTRUCT cs 来修改窗口类或样式
+
+	return CView::PreCreateWindow(cs);
+}
+
+// CMFCApplication1View 绘图
+
+void CMFCApplication1View::OnDraw(CDC* /*pDC*/)
+{
+	CMFCApplication1Doc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	// TODO: 在此处为本机数据添加绘制代码
+}
+
+
+// CMFCApplication1View 诊断
+
+#ifdef _DEBUG
+void CMFCApplication1View::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void CMFCApplication1View::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+CMFCApplication1Doc* CMFCApplication1View::GetDocument() const // 非调试版本是内联的
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CMFCApplication1Doc)));
+	return (CMFCApplication1Doc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CMFCApplication1View 消息处理程序
+
+
+void CMFCApplication1View::OnFileOpen()
+{
+	// TODO: 在此添加命令处理程序代码
+
+	CFileDialog cfd(true);
+	int r = cfd.DoModal();
+	CClientDC dc(this);
+	if (r == IDOK)
+	{
+		CString filename = cfd.GetPathName();
+		CImage img;
+		img.Load(filename);
+		int w, h, sx, sy;
+		{
+
+			CRect rect;
+			GetClientRect(&rect); //取得客户区的大小
+
+			float rect_ratio = 1.0*rect.Width() / rect.Height();//计算客户去的宽高比
+			float img_ratio = 1.0*img.GetWidth() / img.GetHeight();//计算图像的宽高比
+
+			if (rect_ratio > img_ratio)   //客户区宽高比大于图像宽高比
+			{
+				h = rect.Height();         //客户区高度作为图像高度
+				w = img_ratio * h;         //图像宽度维持不变
+				sx = (rect.Width() - w) / 2;
+				sy = 0;
+			}
+			else
+			{
+				w = rect.Width();
+				h = w / img_ratio;
+				sx = 0;
+				sy = (rect.Height() - h) / 2;
+			}
+		}
+		
+		img.Draw(dc.m_hDC, sx, sy, w, h);
+	}
+}
