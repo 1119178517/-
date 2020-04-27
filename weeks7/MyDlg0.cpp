727d0181c33dbd1936ed6f8d881301da09c79633
1119178517 <1119178517@qq.com>
2020-04-27 16:59:18 +0800

    week7.1

---
+++ weeks7/MyDlg0.cpp
@@ -0,0 +1,37 @@
+﻿// MyDlg0.cpp: 实现文件
+//
+
+#include "pch.h"
+#include "weeks7.h"
+#include "MyDlg0.h"
+#include "afxdialogex.h"
+
+
+// MyDlg0 对话框
+
+IMPLEMENT_DYNAMIC(MyDlg0, CDialogEx)
+
+MyDlg0::MyDlg0(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_DIALOG1, pParent)
+{
+
+}
+
+MyDlg0::~MyDlg0()
+{
+}
+
+void MyDlg0::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+	DDX_Text(pDX, IDC_EDIT1, x);
+	DDX_Text(pDX, IDC_EDIT2, y);
+	DDX_Text(pDX, IDC_EDIT3, r);
+}
+
+
+BEGIN_MESSAGE_MAP(MyDlg0, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// MyDlg0 消息处理程序
---
+++ weeks7/weeks7View.cpp
@@ -0,0 +1,104 @@
+﻿
+// weeks7View.cpp: Cweeks7View 类的实现
+//
+
+#include "pch.h"
+#include "framework.h"
+// SHARED_HANDLERS 可以在实现预览、缩略图和搜索筛选器句柄的
+// ATL 项目中进行定义，并允许与该项目共享文档代码。
+#ifndef SHARED_HANDLERS
+#include "weeks7.h"
+#endif
+
+#include "weeks7Doc.h"
+#include "weeks7View.h"
+#include"MyDlg0.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// Cweeks7View
+
+IMPLEMENT_DYNCREATE(Cweeks7View, CView)
+
+BEGIN_MESSAGE_MAP(Cweeks7View, CView)
+	ON_COMMAND(ID_32771, &Cweeks7View::On32771)
+END_MESSAGE_MAP()
+
+// Cweeks7View 构造/析构
+
+Cweeks7View::Cweeks7View() noexcept
+{
+	// TODO: 在此处添加构造代码
+
+}
+
+Cweeks7View::~Cweeks7View()
+{
+}
+
+BOOL Cweeks7View::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: 在此处通过修改
+	//  CREATESTRUCT cs 来修改窗口类或样式
+
+	return CView::PreCreateWindow(cs);
+}
+
+// Cweeks7View 绘图
+
+void Cweeks7View::OnDraw(CDC* /*pDC*/)
+{
+	Cweeks7Doc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	// TODO: 在此处为本机数据添加绘制代码
+}
+
+
+// Cweeks7View 诊断
+
+#ifdef _DEBUG
+void Cweeks7View::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void Cweeks7View::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+Cweeks7Doc* Cweeks7View::GetDocument() const // 非调试版本是内联的
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(Cweeks7Doc)));
+	return (Cweeks7Doc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// Cweeks7View 消息处理程序
+
+
+void Cweeks7View::On32771()
+{
+	// TODO: 在此添加命令处理程序代码
+
+	MyDlg0 dlg;
+	int t = dlg.DoModal();
+	
+	if (t == IDOK)
+	{
+		int R, X, Y;
+		R = dlg.r;
+		X = dlg.x;
+		Y = dlg.y;
+		CRect re(X-R,Y-R,X+R,Y+R);
+		GetDC()->Ellipse(re);
+	}
+
+}
